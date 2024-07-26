36f8ea20662f761f4c9d09b4f6eac67e6539c5a4
Yevgen Vasylenko <yevgenVasylenko93@gmail.com>
2024-07-26 16:59:34 +0300

    Initial Commit

---
+++ NewsDemo/NewsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDemoApp.swift
+//  NewsDemo
+//
+//  Created by Yevgen Vasylenko on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
